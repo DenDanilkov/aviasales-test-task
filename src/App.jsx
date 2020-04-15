d6816da9ef5029d49463d18c4d22cf57a24ea609
Dzianis Danilkou <d.danilkou@godeltech.com>
2020-04-15 20:15:18 +0300

    init: start app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+    </div>
+  );
+}
+
+export default App;
